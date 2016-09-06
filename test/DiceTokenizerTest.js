ab33870fe31b380e7292e24f2681799f06f24353
tvolkov <timofey.volkov@gmail.com>
2016-09-07 01:31:42 +0300

    fixing tests

--- test/DiceTokenizerTest.js
+++ test/DiceTokenizerTest.js
@@ -2,9 +2,9 @@ var chai = require('chai');
 var expect = chai.expect;
 var DiceTokenizer = require('../app/DiceTokenizer.js');
 
-describe("DiceTokenizer", function(){
+describe('DiceTokenizer', function(){
 
-    describe("Should validate input string", function(){
+    describe('Should validate input string', function(){
 
         it('Should throw an Error if input string is undefined', function(){
             var input = undefined;
@@ -25,6 +25,45 @@ describe("DiceTokenizer", function(){
 
             var testDiceTokenizer = new DiceTokenizer(input);
             expect(testDiceTokenizer.tokenize.bind(testDiceTokenizer)).to.throw('invalid expression');
+        });
+
+    });
+
+    describe('Should tokenize input string', function(){
+
+        it('Should procude no operators if input expression doesn not have pluses or minuses', function(){
+            var input = '2d4'
+
+            var testDiceTokenizer = new DiceTokenizer(input);
+            var tokens = testDiceTokenizer.tokenize();
+
+            expect(tokens.operators.length).to.equal(0);
+            expect(tokens.operands.length).to.equal(1);
+            expect(tokens.operands).to.contain('2d4');
+        });
+
+        it('Should put pluses and minuses to operators array', function(){
+            var input = '2d4 + 3'
+
+            var testDiceTokenizer = new DiceTokenizer(input);
+            var tokens = testDiceTokenizer.tokenize();
+
+            expect(tokens.operators.length).to.equal(1);
+            expect(tokens.operators).to.contain('+');
+            expect(tokens.operands.length).to.equal(2);
+            expect(tokens.operands).to.contain('2d4');
+            expect(tokens.operands).to.contain('3');
+        });
+
+        it('Should treat percent sign as 100', function(){
+            var input = 'd%';
+
+            var testDiceTokenizer = new DiceTokenizer(input);
+            var tokens = testDiceTokenizer.tokenize();
+
+            expect(tokens.operators.length).to.equal(0);
+            expect(tokens.operands.length).to.equal(1);
+            expect(tokens.operands).to.contain('d100');
         });
     });
 });
