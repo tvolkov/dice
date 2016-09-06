00f2c1b21fb6af93affcc0d8b03ec5710ffdcc12
tvolkov <timofey.volkov@gmail.com>
2016-09-07 02:02:36 +0300

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# dice expression evaluation
+
+current issues: 
+1. need to fix a regex for validating expressions.
+2. need to mock random-js for unit tests
--- test/ExpressionTreeBuilderTest.js
+++ test/ExpressionTreeBuilderTest.js
@@ -2,3 +2,24 @@ var chai = require('chai');
 var expect = chai.expect;
 
 var ExpressionTreeBuilder = require('../app/ExpressionTreeBuilder.js');
+
+describe('ExpressionTreeBuilder', function(){
+    it('Should build a tree from tokens arrays', function(){
+        var input = {operands: ['4d6', '7'], operators: ['+']}
+
+        var expressionTreeBuilder = new ExpressionTreeBuilder(input);
+        var result = expressionTreeBuilder.buildTree();
+
+        expect(result.value).to.equal('+');
+        expect(result.left).to.equal('4d6');
+        expect(result.right).to.equal('7');
+    });
+
+    it('Should throw an error if tokens are null', function(){
+        var input = undefined;
+
+        var expressionTreeBuilder = new ExpressionTreeBuilder(input);
+
+        expect(expressionTreeBuilder.buildTree.bind(expressionTreeBuilder)).to.throw('no tokens found');
+    });
+});
