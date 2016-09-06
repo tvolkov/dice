512189e47a9026a9d097198818f50a1276819d36
tvolkov <timofey.volkov@gmail.com>
2016-09-06 19:16:52 +0300

    fixing treebuilder

--- ExpressionTreeBuilder.js
+++ ExpressionTreeBuilder.js
@@ -3,45 +3,34 @@ function ExpressionTreeBuilder(tokens){
 }
 
 ExpressionTreeBuilder.prototype.buildTree = function(){
-    console.log('building expression tree');
     var operands = this.tokens.operands;
     var operators = this.tokens.operators;
 
     var currentOperand = 0;
 
-    var tree = {value: undefined, left: undefined, right: undefined};
+    var tree;
 
     for (var i = 0; i < operators.length; i++){
-        // var treeNode = operators[i];
-        // treeNode.left = operands[currentOperand++];
-        // treeNode.right = operands[currentOperand++]
-        // if (typeof tree == 'undefined'){
-        //     tree = treeNode;
-        // } else {
-        //     tree.left = treeNode;
-        // }
 
-        //--------------------------------------------
         var treeNode = {value: operators[i], left: undefined, right: undefined};
-        if (typeof tree.value == 'undefined' && typeof tree.left == 'undefined' && typeof tree.right == 'undefined'){
-            console.log('tree is undefined');
+
+        if (typeof tree == 'undefined'){
             treeNode.left = operands[currentOperand++];
             treeNode.right = operands[currentOperand++]
             tree = treeNode;
-            console.log('treenode'+treeNode);
         } else {
             if (treeNode.value == 'd'){
                 treeNode.left = operands[currentOperand - 1];
                 treeNode.right = operands[currentOperand++]
                 tree.right = treeNode;
+            } else {
+                //JSON.parse(JSON.stringify(jsonObject)); is used to clone subtrees
+                var tmpSubTree = JSON.parse(JSON.stringify(tree));
+                treeNode.left = tmpSubTree;
+                treeNode.right = operands[currentOperand++];
+                tree = treeNode    
             }
-            var tmpSubTree = tree;
-            treeNode.left = tmpSubTree;
-            treeNode.right = operands[currentOperand++];
-            tree = treeNode
         }
-
-
     }
 
     console.log(tree);
